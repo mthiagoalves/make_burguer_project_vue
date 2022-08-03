8ebdaf809e73f14a01e0ea8fdc739294ea125c30
mthiagoalves <m.thiago.alves@live.com>
2022-08-03 11:39:05 +0100

    feat(requestView): implement dashboard frontend

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,98 @@
+<template>
+  <div id="burguer-table">
+    <div>
+      <div id="burguer-table-heading">
+        <div class="order-id">#:</div>
+        <div>client</div>
+        <div>bread</div>
+        <div>beef</div>
+        <div>opcionals</div>
+        <div>actions</div>
+      </div>
+    </div>
+    <div id="burguer-table-rows">
+      <div class="burguer-table-row">
+        <div class="order-number">1</div>
+        <div>name</div>
+        <div>bread</div>
+        <div>beef</div>
+        <div>
+          <ul>
+            <li>opcionals</li>
+            <li>opcionals</li>
+          </ul>
+        </div>
+        <div>
+          <select name="status" class="status">
+            <option value="">Select status</option>
+          </select>
+          <button class="delete-btn">Cancel</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Dashboard",
+};
+</script>
+
+<style scoped>
+#burguer-table {
+  max-width: 1200px;
+  margin: 0 auto;
+}
+
+#burguer-table-heading,
+#burguer-table-rows,
+.burguer-table-row {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+#burguer-table-heading {
+  font-weight: bold;
+  padding: 12px;
+  border-bottom: 2px solid #333;
+}
+
+#burguer-table-heading div,
+.burguer-table-row div {
+  width: 19%;
+}
+
+.burguer-table-row {
+  width: 100%;
+  padding: 12px;
+  border-bottom: 1px solid #ccc;
+}
+
+#burguer-table-heading .order-id,
+.burguer-table-row .order-number {
+  width: 5%;
+}
+
+select {
+  padding: 12px 6px;
+  margin-right: 12px;
+}
+
+.delete-btn {
+  background-color: #222;
+  color: #fcba03;
+  font-weight: bold;
+  border: 2px solid #222;
+  padding: 10px;
+  font-size: 16px;
+  margin: 0 auto;
+  cursor: pointer;
+  transition: 0.5s;
+}
+
+.delete-btn:hover {
+  background-color: transparent;
+  color: #222;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -8,12 +8,12 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/pedidos',
-    name: 'pedidos',
+    path: '/requests',
+    name: 'requests',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/PedidosView.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/RequestsView.vue')
   }
 ]
 
